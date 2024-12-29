2edd422dcc021f8992c310901a333e69c5d8db6e
Lohan Yrvine <o.lohan.yrvine@gmail.com>
2024-12-29 18:24:59 -0300

    2024: day 05 using topological sort

--- 2024/day05/main.go
+++ 2024/day05/main.go
@@ -8,77 +8,58 @@ import (
 	"github.com/leakedmemory/aoc/helpers"
 )
 
-type rulesMap map[string]map[string]bool
+func parseInput(input string) (map[int][]int, [][]int) {
+	sections := strings.Split(input, "\n\n")
+	rules := strings.Split(sections[0], "\n")
+	updates := strings.Split(sections[1], "\n")
 
-func parseInput(input string) (rulesMap, []string) {
-	tmp := strings.Split(input, "\n\n")
-	rules := strings.Split(tmp[0], "\n")
-	updates := strings.Split(tmp[1], "\n")
+	// parse rules into a directed graph
+	graph := make(map[int][]int)
+	for _, rule := range rules {
+		parts := strings.Split(rule, "|")
+		x, _ := strconv.Atoi(parts[0])
+		y, _ := strconv.Atoi(parts[1])
+		graph[x] = append(graph[x], y)
+	}
 
-	rMap := makeRulesMap(rules)
+	// parse updates into int slices
+	updateList := [][]int{}
+	for _, update := range updates {
+		pages := strings.Split(update, ",")
+		parsed := []int{}
+		for _, p := range pages {
+			page, _ := strconv.Atoi(p)
+			parsed = append(parsed, page)
+		}
+		updateList = append(updateList, parsed)
+	}
 
-	return rMap, updates
+	return graph, updateList
 }
 
-func part1(rMap rulesMap, updates []string) int {
+func part1(graph map[int][]int, updates [][]int) int {
 	result := 0
 	for _, update := range updates {
-		u := strings.Split(update, ",")
-		if isCorrectOrder(rMap, u) {
-			mid, _ := strconv.Atoi(u[len(u)/2])
-			result += mid
+		if isCorrectOrder(graph, update) {
+			result += update[len(update)/2]
 		}
 	}
 
 	return result
 }
 
-func makeRulesMap(rules []string) rulesMap {
-	m := rulesMap{}
-	for _, r := range rules {
-		tmp := strings.Split(r, "|")
-		left := tmp[0]
-		right := tmp[1]
-
-		if m[left] == nil {
-			m[left] = make(map[string]bool)
-		}
-		if m[right] == nil {
-			m[right] = make(map[string]bool)
-		}
-
-		// false == parent value should come before child value
-		m[left][right] = false
-		// true == parent value should come after child value
-		m[right][left] = true
+func isCorrectOrder(graph map[int][]int, update []int) bool {
+	pos := make(map[int]int)
+	for i, page := range update {
+		pos[page] = i
 	}
 
-	return m
-}
-
-func isCorrectOrder(rMap rulesMap, update []string) bool {
-	for i, page := range update {
-		rules, ok := rMap[page]
-		if !ok {
-			continue
-		}
-
-		for j, p := range update {
-			if p == page {
-				continue
-			}
-
-			after, ok := rules[p]
-			if !ok {
-				continue
-			}
-
-			if after && i < j {
-				return false
-			}
-
-			if !after && j < i {
-				return false
+	for x, ys := range graph {
+		if xIdx, ok := pos[x]; ok {
+			for _, y := range ys {
+				if yIdx, ok := pos[y]; ok && xIdx > yIdx {
+					return false
+				}
 			}
 		}
 	}
@@ -86,90 +67,80 @@ func isCorrectOrder(rMap rulesMap, update []string) bool {
 	return true
 }
 
-func part2(rMap rulesMap, updates []string) int {
+func part2(graph map[int][]int, updates [][]int) int {
 	result := 0
 	for _, update := range updates {
-		u := strings.Split(update, ",")
-		if !isCorrectOrder(rMap, u) {
-			fixed := fixUpdateOrder(rMap, u)
-			mid, _ := strconv.Atoi(fixed[len(fixed)/2])
-			result += mid
+		if !isCorrectOrder(graph, update) {
+			sorted := topologicalSort(graph, update)
+			result += sorted[len(sorted)/2]
 		}
 	}
 
 	return result
 }
 
-// probably should not take 20s to execute, but works for now.
-func fixUpdateOrder(rMap rulesMap, update []string) []string {
-	fixed := update
-Outer:
-	for {
-		for i, page := range fixed {
-			rules, ok := rMap[page]
-			if !ok {
-				continue
-			}
+func topologicalSort(graph map[int][]int, update []int) []int {
+	pos := make(map[int]int)
+	for i, page := range update {
+		pos[page] = i
+	}
 
-			for j, p := range fixed {
-				if p == page {
-					continue
-				}
-
-				after, ok := rules[p]
-				if !ok {
-					continue
-				}
-
-				if after && i < j {
-					fixed = moveToIndex(fixed, i, j+1)
-					if isCorrectOrder(rMap, fixed) {
-						return fixed
-					}
-
-					continue Outer
-				}
-
-				if !after && j < i {
-					fixed = moveToIndex(fixed, j, i+1)
-					if isCorrectOrder(rMap, fixed) {
-						return fixed
-					}
-
-					continue Outer
+	inDegree := make(map[int]int)
+	for x, ys := range graph {
+		if _, ok := pos[x]; ok {
+			for _, y := range ys {
+				if _, ok := pos[y]; ok {
+					inDegree[y]++
 				}
 			}
 		}
 	}
-}
 
-func moveToIndex(update []string, from, to int) []string {
-	result := make([]string, 0, len(update))
-	copy(result, update)
-
-	if from < to {
-		result = append(result, update[:from]...)
-		result = append(result, update[from+1:to]...)
-		result = append(result, update[from])
-		if to <= len(update)-1 {
-			result = append(result, update[to:]...)
+	// initialize queue with nodes that have no dependencies
+	queue := []int{}
+	for _, page := range update {
+		if inDegree[page] == 0 {
+			queue = append(queue, page)
 		}
-		return result
-	} else {
-		result := update[:to]
-		result = append(result, update[from])
-		result = append(result, update[to+1:from]...)
-		if from <= len(update)-1 {
-			result = append(result, update[from:]...)
-		}
-		return result
 	}
+
+	sorted := []int{}
+	visited := make(map[int]bool)
+	for len(queue) > 0 {
+		minPos := len(update)
+		minIdx := 0
+		for i, node := range queue {
+			if pos[node] < minPos {
+				minPos = pos[node]
+				minIdx = i
+			}
+		}
+
+		node := queue[minIdx]
+		queue = append(queue[:minIdx], queue[minIdx+1:]...)
+
+		if !visited[node] {
+			sorted = append(sorted, node)
+			visited[node] = true
+
+			for _, neighbor := range graph[node] {
+				if !visited[neighbor] {
+					inDegree[neighbor]--
+					if inDegree[neighbor] == 0 {
+						queue = append(queue, neighbor)
+					}
+				}
+			}
+		}
+	}
+
+	return sorted
 }
 
 func main() {
 	input := helpers.ReadFile("2024/day05/input.txt")
 
-	rMap, updates := parseInput(input)
-	fmt.Printf("part1: %v\n", part1(rMap, updates))
-	fmt.Printf("part2: %v\n", part2(rMap, updates))
+	graph, updates := parseInput(input)
+	fmt.Printf("part1: %v\n", part1(graph, updates))
+	fmt.Printf("part2: %v\n", part2(graph, updates))
 }
